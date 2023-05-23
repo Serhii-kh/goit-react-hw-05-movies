f20aaf15b89f0f9908a97dc44330274b4da01bd4
Serhii Naboka <drifterzzz@gmail.com>
2023-05-23 22:32:51 +0300

    upd

--- src/components/pages/MovieDetails/MovieDetails.jsx
+++ src/components/pages/MovieDetails/MovieDetails.jsx
@@ -1,18 +1,26 @@
 import { fetchMovieById } from "components/Api/fetchMovies"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export const MovieDetails = () => {
+	const [ movie, setMovie ] = useState(null)
 	const { movieId } = useParams()
 	console.log(movieId)
 
 	useEffect(() => {
 		fetchMovieById(movieId)
+			.then(movie => {
+				setMovie(movie)
+				console.log(movie)
+			})
 	}, [movieId])
 
 	return (
 		<>
-			<div>{movieId}</div>
+			
+			<div className="movieDetails">
+				<h1>axaxa</h1>
+			</div>
 		</>
 	)
 }
