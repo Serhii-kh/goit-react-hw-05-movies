5b4e8902945e53930c401eb92e86891f04f256ce
Serhii Naboka <drifterzzz@gmail.com>
2023-05-23 23:19:48 +0300

    upd

--- src/components/pages/MovieDetails/MovieDetails.jsx
+++ src/components/pages/MovieDetails/MovieDetails.jsx
@@ -3,28 +3,43 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export const MovieDetails = () => {
-	const [movie, setMovie] = useState({})
+	const [movie, setMovie] = useState(null)
 	const { movieId } = useParams()
 
 	useEffect(() => {
-		fetchMovieById(movieId)
-			.then(({ data }) => {
-				setMovie(data)
-				console.log(movie)
-				console.log(data)
-			})
+		try {
+			fetchMovieById(movieId)
+				.then(data => {
+					setMovie(data)
+					console.log(movie)
+					console.log(data)
+				})
+		}
+		catch (error) {
+			console.log(error)
+		}
+		finally { }
+
 	}, [movieId])
 
 	const { original_title, overview, genres } = movie
 
+	const movieGenres = []
+
+	if (genres) {
+		for (const genre of genres) {
+			movieGenres.push(genre.name)
+		}
+	}
+
 	return (
 		<>
-			
+
 			<div className="movieDetails">
 				<h1>{original_title}</h1>
-				{/* <p>{genres }</p> */}
+				<p>{movieGenres}</p>
 				<p>{overview}</p>
-				
+
 			</div>
 		</>
 	)
