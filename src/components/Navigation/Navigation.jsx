8b40d3e07e42ac6517955d835bae57ba869f9a88
Serhii Naboka <drifterzzz@gmail.com>
2023-05-27 16:26:58 +0300

    add Active-link css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,14 +2,16 @@ import { NavLink } from "react-router-dom";
 import css from '../Navigation/Navigation.module.css'
 
 export const Navigation = () => {
+	const setActive = ({ isActive }) => isActive ? 'active-link' : ''
+
 	return (
 		<>
 			<header>
 				<nav className={css.Navigation}>
-					<NavLink className={css.Navigation__link} to="/">Home</NavLink>
-					<NavLink className={css.Navigation__link} to="search/movie">Movies</NavLink>
+					<NavLink to="/" className={setActive}>Home</NavLink>
+					<NavLink to="search/movie" className={setActive}>Movies</NavLink>
 				</nav>
-		</header>
+			</header>
 		</>
 	)
 }
